8e67fc176e7f58262dfb1e39cce14117b0b7ae05
Rebekah Kang <rbkh13@gmail.com>
2020-02-27 10:15:49 -0500

    fixed calendar previous/next month

--- calendar.js
+++ calendar.js
@@ -1,4 +1,5 @@
 window.onload = function(){
+
     var d = new Date();
     var month_name = ['January','February','March','April','May','June','July','August','September','October','November','December'];
     var month = d.getMonth();   //0-11
@@ -6,49 +7,64 @@ window.onload = function(){
     var first_date = month_name[month] + " " + 1 + " " + year;
     //February 1 2020
     var tmp = new Date(first_date).toDateString();
-    //Sat Feb 01 2020 (first day of current month))
+    //Sat Feb 01 2020 
     var first_day = tmp.substring(0, 3);    //Sat
     var day_name = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
     var day_no = day_name.indexOf(first_day);   //1
     var days = new Date(year, month+1, 0).getDate();    //29
-    //Tue Feb 29 2020 (last day of current month)
+    //Tue Feb 29 2020 
     var calendar = get_calendar(day_no, days);
     document.getElementById("calendar-month-year").innerHTML = month_name[month] + " " + year;
-    document.getElementById("calendar-dates").appendChild(calendar);
+    var display = document.getElementById("calendar-dates").appendChild(calendar);
 
     //previous month
     document.getElementById("btnPrevMonth").onclick = function() {
         var curr_month = month; 
         var curr_year = year;
-        if (month == 0) {
+        var cnt = 0;
+        if(month == 0) {
             curr_month = 11;
-            curr_month--;
             curr_year--;        
         } else {
             curr_month--;
         }
       
         document.getElementById("calendar-month-year").innerHTML = month_name[curr_month] + " " + curr_year;
-        document.getElementById("calendar-dates").appendChild(calendar);
         month = curr_month;
         year = curr_year;
+
+        first_date = month_name[month] + " " + 1 + " " + year;
+        tmp = new Date(first_date).toDateString();
+        first_day = tmp.substring(0, 3);
+        day_no = day_name.indexOf(first_day); 
+        days = new Date(year, month+1, 0).getDate();      
+        calendar = get_calendar(day_no, days);
+        var prev = document.getElementById("calendar-dates").appendChild(calendar);
+        prev.previousSibling.remove();
+      
     };
 
     //next month
     document.getElementById("btnNextMonth").onclick = function() {
         var curr_month = month; 
         var curr_year = year;
-        if (month == 11) {
+        if(month == 11) {
             curr_month = 0;
-            curr_month++;
             curr_year++;
         } else {
             curr_month++;
         }
         document.getElementById("calendar-month-year").innerHTML = month_name[curr_month] + " " + curr_year;
-        document.getElementById("calendar-dates").appendChild(calendar);
         month = curr_month;
         year = curr_year;
+        first_date = month_name[month] + " " + 1 + " " + year;
+        tmp = new Date(first_date).toDateString();
+        first_day = tmp.substring(0, 3);
+        day_no = day_name.indexOf(first_day); 
+        days = new Date(year, month+1, 0).getDate(); 
+        calendar = get_calendar(day_no, days);
+        var next = document.getElementById("calendar-dates").appendChild(calendar);
+        next.previousSibling.remove();
     };
 }
 
