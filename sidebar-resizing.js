678f89f504f6a7f51c2206065378ca98fba733ee
Rebekah Kang <rbkh13@gmail.com>
2020-03-04 15:43:28 -0500

    working on sidebar responsiveness

---
+++ sidebar-resizing.js
@@ -0,0 +1,20 @@
+function openNav() {
+    $(".openBtn").click(function(){
+        $(".sidebar").toggle();
+    });
+}
+
+/*Needs close button too*/ 
+
+
+/*
+function openNav() {
+    var open = document.getElementsByClassName("filters");
+    if (open.style.display === "none") {
+        open.style.display = document.getElementsByClassName("filters");
+    } 
+    else {
+        open.style.display = "none";
+    }
+}
+*/
