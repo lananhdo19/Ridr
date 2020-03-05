bd44fc898a01c6106f6d39fe94ce82da3f99e136
zmeherin <zmeherin@gmail.com>
2020-03-04 20:45:23 -0500

    some changes

--- create-post.js
+++
@@ -1,6 +0,0 @@
-var modal = document.getElementById("create-post-modal-form");
-
-function createPostFunction() {
-    alert("The form was submitted");
-    console.log("The form was submitted");
-}
