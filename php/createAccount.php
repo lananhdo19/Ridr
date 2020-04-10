6540009ae7a4f375134dc4db8cbbf24a670b5e9e
zmeherin <zmeherin@gmail.com>
2020-04-10 15:38:56 -0400

    testing on local branch

--- php/createAccount.php
+++ php/createAccount.php
@@ -6,17 +6,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $password = password_hash(htmlspecialchars($_POST['password']), PASSWORD_BCRYPT);
     $first_name = htmlspecialchars($_POST['first_name']);
     $last_name = htmlspecialchars($_POST['last_name']);
-    $profile_img = $_POST['profile-img'];
-
-    if($_FILES['profile-img']['name'])
-    {
-        $save_path="static/profile-imgs/"; // moving the image
-        $myname = strtolower($_FILES['image']['tmp_name']); //You are renaming the file here
-        move_uploaded_file($_FILES['image']['tmp_name'], $save_path.$myname); // Move the uploaded file to the desired folder
-    }
-
-    $inser_into_db="INSERT INTO `database`.`table` (`folder_name`, `file_name`) VALUES('$save_path', '$myname'))";
-
 
     $query = "INSERT INTO user VALUES (:email, :password, :first_name, :last_name)";
     $statement = $db -> prepare($query);
