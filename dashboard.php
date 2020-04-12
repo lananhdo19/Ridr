7fc4e40b781a69b6faf9a7697aa9a1d18d1b8424
lananhdo19 <36138936+lananhdo19@users.noreply.github.com>
2020-04-12 16:44:51 -0400

    Expand Post Functionality
    With login css

--- dashboard.php
+++ dashboard.php
@@ -39,11 +39,11 @@ $userposts = getUserPosts();
     <!-- Column for past posts and requests -->
     <div class="history col-6">
         <div class="post-listings mainPost">
-            <h1 class="mega-header">Current Rides</h2>
+            <h1 class="mega-header">Current Rides</h1>
             <br>
             <?php include('dashboard/current-posts.php') ?>
             <br>
-            <h1 class="mega-header">Past Rides</h2>
+            <h1 class="mega-header">Past Rides</h1>
             <br/>
             <?php include('dashboard/past-posts.php') ?>
         </div>
@@ -52,75 +52,9 @@ $userposts = getUserPosts();
     <!-- Column for information on a specifc post listing the riders -->
     <div class="expandedPostColumn col-3">
         <div class="expandedPostContainer">
-                <div class="expandedPost">
-                    <div class="expandedPost-Body">
-                    <h4>Information about Post</h4>
-                    <p>
-                        {{Destination}}<br/>
-                        {{Date}}<br/>
-                        {{Time}}<br/>
-                        {{Comments}}
-                    </p>
-                    </div>
+            <div class="expandedPost">
 
-
-                    <!-- Table for riders listing returned from database and edited by DOM JS -->
-                    <table class="expandedPostRiders">
-                        <tbody>
-                            <tr>
-                                <div class="riders-body">
-                                    <hr>
-                                    <p class="panel-p">
-                                        {{Rider Name}}<br/>
-                                        {{Rider Contact}}<br/>
-                                        {{Comments}}
-                                    </p>
-                                </div>
-                            </tr>
-                            <tr>
-                                <div class="riders-body">
-                                    <hr>
-                                    <p class="panel-p">
-                                        {{Rider Name}}<br/>
-                                        {{Rider Contact}}<br/>
-                                        {{Comments}}
-                                    </p>
-                                </div>
-                            </tr>
-                            <tr>
-                                <div class="riders-body">
-                                    <hr>
-                                    <p class="panel-p">
-                                        {{Rider Name}}<br/>
-                                        {{Rider Contact}}<br/>
-                                        {{Comments}}
-                                    </p>
-                                </div>
-                            </tr>
-                            <tr>
-                                <div class="riders-body">
-                                    <hr>
-                                    <p class="panel-p">
-                                        {{Rider Name}}<br/>
-                                        {{Rider Contact}}<br/>
-                                        {{Comments}}
-                                    </p>
-                                </div>
-                            </tr>
-                            <tr>
-                                <div class="riders-body">
-                                    <hr>
-                                    <p class="panel-p">
-                                        {{Rider Name}}<br/>
-                                        {{Rider Contact}}<br/>
-                                        {{Comments}}
-                                    </p>
-                                </div>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-        </div>
+            </div>
 
     </div>
 
