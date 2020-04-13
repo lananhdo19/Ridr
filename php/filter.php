673880a79df28880bce2dde7d739653f28ed90e3
Rebekah Kang <rbkh13@gmail.com>
2020-04-13 00:57:04 -0400

    working on filters

--- php/filter.php
+++ php/filter.php
@@ -1,23 +1,31 @@
 <?php
 require('connectdb.php');
 
-include('posts/create-post.php');
-
 if(isset($_GET['submit'])){
+    session_start();
+
     $date = $_GET['date'];
     $from_time = $_GET['from_time']; //time: 6:04pm->18:04
     $to_time = $_GET['to_time'];
     $zipcode = $_GET['zipcode'];
-    $isDriver = $_GET['isDriver'];
+    $isDriver = $_GET['isDriverRider'];
     
     $sql = "SELECT * FROM post WHERE true";
     
+    /*
     if($isDriver == "driver") {
         $sql .= " AND isDriver='0'";
     }
     if($isDriver == "rider") {
         $sql .= " AND isDriver='1'";
+    }*/
+
+    if(isset($_GET['isDriverRider'])){
+        $isDriver = $_GET['isDriverRider']; 
+    }else{
+        $isDriver = "Something's wrong bro ";
     }
+
     if($date != "") {
         $sql .= " AND DATE(datetime)='$date'";
     }
@@ -31,7 +39,11 @@ if(isset($_GET['submit'])){
     $query = $db->prepare($sql);
     $query->execute();
     $results = $query->fetchAll( PDO::FETCH_ASSOC ); 
+    
+    //header('Location: Ridr/posts.php');
     print_r($results);  
+
+    //make sure it doesn't give form resubmission on page refresh
     
 }
 
