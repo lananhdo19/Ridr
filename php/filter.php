8ea9285f6e15918a89b57abc73568333e3ce084a
Rebekah Kang <rbkh13@gmail.com>
2020-04-14 01:26:36 -0400

    fixed placement of button

--- php/filter.php
+++ php/filter.php
@@ -50,46 +50,60 @@ if(isset($_GET['submit'])){
 
 ?>
 
-<div class="main">
-    <div class="flex-container-wrap panels">
-        <?php foreach ($posts as $post):  ?>
-            <div class="post-listings" id="post-listings">
-                <div class="panel panel-default">
-                    <div class="panel-body">
+<div class="flex-container-wrap panels">
+    <!-- Ride Listings -->
+    <div class="post-listings" id="post-listings">
+    <?php foreach ($posts as $post):  ?> <!-- iterating over each row in post table -->
+            <!-- Listing -->
+            <div class="panel panel-default">
+                <div class="panel-body">
                         <div class="panel-vertical-top" style="font-weight:400;">
-                            <!-- <img class="profile-pic" src=
-                                <?php echo "'" . $post['profile-pic'] . "'"; ?>
-                            > -->
-                            <img src="static/images/sail.jfif" class="profile-pic">
-                            <div class="normal-text">
-                                <p style="font-weight:500;">
-                                    <?php
-                                        if ($post['isDriver'] == 0) { echo "Giving a ride"; }
-                                        else { echo "Looking for a ride"; }
-                                    ?>
-                                    <br/>
-                                    <span style="color:darkgrey; font-size:14px;">
-                                        <?php echo getUsername($post['email']); ?>
-                                    </span>
-                                </p>
+                                <!-- <img class="profile-pic" src=
+                                    <?php echo "'" . $post['profile-pic'] . "'"; ?>
+                                > -->
+                                <img src="static/images/sail.jfif" class="profile-pic">
+                                <div class="normal-text">
+                                    <p style="font-weight:500;">
+                                        <?php
+                                            if ($post['isDriver'] == 0) { echo "Giving a ride"; }
+                                            else { echo "Looking for a ride"; }
+                                        ?>
+                                        <br/>
+                                        <span style="color:darkgrey; font-size:14px;">
+                                            <?php echo getUsername($post['email']); ?>
+                                        </span>
+                                    </p>
 
-                                <?php echo "<span style='font-weight:500;'>" . "Location:  " . "</span>" . $post['destination']; ?>
-                                <br/>
-                                <?php formatDateAndTime($post['datetime']); ?>
-                                <br/>
-                                <?php if ($post['comment'] != "") echo "<span style='font-weight:500;'>" . "Comments:  " . "</span>" . $post['comment']; ?>
-                            </div>
+                                    <?php echo "<span style='font-weight:500;'>" . "Location:  " . "</span>" . $post['destination']; ?>
+                                    <br/>
+                                    <?php formatDateAndTime($post['datetime']); ?>
+                                    <br/>
+                                    <?php if ($post['comment'] != "") echo "<span style='font-weight:500;'>" . "Comments:  " . "</span>" . $post['comment']; ?>
+                                </div>
                         </div>
-                    </div>
+                    <!-- Take/Give Ride Buttons -->
+                    <?php 
+                        if ($post['isDriver'] == 0) {
+                            echo '<form action="posts/take-ride.php" method="post" class="takegive_form">';
+                            echo '<input type="hidden" name="post_ID" value="' . $post['post_ID'] . '">';
+                            echo '<input type="hidden" name="email" value="' . $post['email'] . '">';
+                            echo '<button type="submit" class="request_button subheader right-div-button"  
+                                          id="' . $post['post_ID'] . '">Take Ride</button>';
+                            echo '</form>';
+                        }
+                        else {
+                            echo '<form action="posts/give-ride.php" method="post" class="takegive_form">';
+                            echo '<input type="hidden" name="post_ID" value="' . $post['post_ID'] . '">';
+                            echo '<input type="hidden" name="email" value="' . $post['email'] . '">';
+                            echo '<button type="submit" class="offer_button subheader right-div-button"  
+                                          id="' . $post['post_ID'] . '">Give Ride</button>';
+                            echo '</form>';
+                        }
+                    ?>
                 </div>
-                <?php 
-                    if ($post['isDriver'] == 0) 
-                        echo '<button type="button" class="request_button subheader right-div-button">Take Ride</button>';
-                    else
-                        echo '<button type="button" class="offer_button subheader right-div-button">Give Ride</button>';
-                ?>
             </div>
+            <!-- Listing -->
         <?php endforeach; ?>
     </div>
 </div>
-        <?php } ?>
+<?php } ?>
